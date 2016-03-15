7c05d1d8a04e47328ddef82c7ecc3e291af1364d
werts <werts-we@qq.com>
2016-03-15 16:50:13 +0800

    modified:   index.js 	modified:   package.json

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * 在postprocessor阶段对属于module模块化组件的代码包裹上define
+ * @param {Object} content
+ * @param {Object} file
+ * @param {Object} conf
+ */
+module.exports = function(content, file, conf){
+    if (file.isMod || conf.wrapAll){
+        if (!/^\s*define\s*\(/.test(content)){
+            content = 'define(function(require, exports, module){' + content + '})';
+        }
+        return content;
+    }
+}
